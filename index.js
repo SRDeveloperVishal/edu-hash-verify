ae06a6d85b7a4b6913cc7da70d20cdd49d47ee2e
ShubhamKarala <shubhamkarala@gmail.com>
2020-10-11 01:44:16 +0530

    some few changes

--- index.js
+++ index.js
@@ -46,7 +46,8 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5001;
+const HOST = process.env.HOST || 'http://127.0.0.1';
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT} and using ${provider} endpoint!`));
+app.listen(PORT, HOST, () => console.log(`Server started on port ${PORT} and using ${provider} endpoint!`));
 
 app.locals.env = process.env;
